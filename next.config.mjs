b67ccdbc1cb7909eea1c1b000f615fe822daf408
tonokim <tonokim@qq.com>
2024-07-08 21:40:17 +0800

    docker

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig = {
+  output: "standalone",
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
